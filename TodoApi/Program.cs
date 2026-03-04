b7bd847e344df47ce5a6a675bf4e0335f34e43e6
simonavegatel <fran.garcia@navegatel.es>
2026-03-04 11:41:50 +0100

    Agregar archivos de proyecto.

---
+++ TodoApi/Program.cs
@@ -0,0 +1,75 @@
+using MongoDB.Driver;
+using TodoApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connectionString = "mongodb+srv://Emilio:1234@cluster0.cce9s1o.mongodb.net/";
+var mongoClient = new MongoClient(connectionString);
+var database = mongoClient.GetDatabase("TodoAppDb");
+var todoCollection = database.GetCollection<Todo>("Tareas");
+
+builder.Services.AddSingleton(todoCollection);
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1");
+    c.RoutePrefix = string.Empty;
+});
+
+app.UseCors();
+
+var todoItems = app.MapGroup("/todoitems");
+
+todoItems.MapGet("/", async (IMongoCollection<Todo> col) =>
+    await col.Find(_ => true).ToListAsync());
+
+todoItems.MapGet("/{id:int}", async (int id, IMongoCollection<Todo> col) =>
+    await col.Find(t => t.TareaCodigo == id).FirstOrDefaultAsync()
+        is Todo todo ? Results.Ok(todo) : Results.NotFound());
+
+todoItems.MapPost("/", async (Todo todo, IMongoCollection<Todo> col) =>
+{
+    todo.Id = null;
+    var ultimaTarea = await col.Find(_ => true).SortByDescending(t => t.TareaCodigo).FirstOrDefaultAsync();
+    todo.TareaCodigo = (ultimaTarea == null) ? 1 : ultimaTarea.TareaCodigo + 1;
+    todo.CreadoEn = DateTime.UtcNow;
+
+    await col.InsertOneAsync(todo);
+    return Results.Created($"/todoitems/{todo.TareaCodigo}", todo);
+});
+
+todoItems.MapPut("/{id:int}", async (int id, Todo inputTodo, IMongoCollection<Todo> col) =>
+{
+    var tareaExistente = await col.Find(t => t.TareaCodigo == id).FirstOrDefaultAsync();
+    if (tareaExistente == null) return Results.NotFound();
+
+    inputTodo.Id = tareaExistente.Id;
+    inputTodo.TareaCodigo = id;
+
+    var result = await col.ReplaceOneAsync(t => t.TareaCodigo == id, inputTodo);
+    return result.ModifiedCount > 0 ? Results.NoContent() : Results.NotFound();
+});
+
+todoItems.MapDelete("/{id:int}", async (int id, IMongoCollection<Todo> col) =>
+{
+    var result = await col.DeleteOneAsync(t => t.TareaCodigo == id);
+    return result.DeletedCount > 0 ? Results.NoContent() : Results.NotFound();
+});
+
+app.Run();
---
+++ TodoApi/Todo.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TodoApi;
+
+public class Todo
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    public int TareaCodigo { get; set; }
+
+    public string? Name { get; set; }
+    public string? Nota { get; set; }
+    public bool Done { get; set; }
+    public string? Tipo { get; set; }
+    public DateTime CreadoEn { get; set; }
+    public DateTime? FinalizadoEn { get; set; }
+    public DateTime? FechaCompletado { get; set; }
+    public List<Subtarea> Subtareas { get; set; } = new();
+}
+
+public class Subtarea
+{
+    public string? Nombre { get; set; }
+    public bool Completada { get; set; }
+}
